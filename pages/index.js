e39110e1e045c1ca7a89531b177207f7ac67e710
Kate-chernyuk <76167446+Kate-chernyuk@users.noreply.github.com>
2021-06-09 11:41:24 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,12 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
+            <Image
+              src="/me.png"
+              alt="Picture of the author"
+              width={500}
+              height={500}
+             />
             <h3>Wiki &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
