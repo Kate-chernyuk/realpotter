d1871c8a653c278a4d45e6d2406fe64f41621422
Kate-chernyuk <76167446+Kate-chernyuk@users.noreply.github.com>
2021-06-09 20:13:54 +0300

    Create article.js

---
+++ components/article.js
@@ -0,0 +1,11 @@
+import styles from '../styles/blog.module.css'
+
+export default function Article({children}) {
+  return(
+    <>
+    <div className={styes.article}>
+        {children}
+    </div>
+    </>
+   )
+}
