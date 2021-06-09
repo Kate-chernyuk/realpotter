af279247f8d4fad82900e035fe1599c80e27a2fd
Kate-chernyuk <76167446+Kate-chernyuk@users.noreply.github.com>
2021-06-09 19:19:50 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <img src="/hog.jpg" class="background" />
+      <img src="/hog.jpg" alt="Hogwarts" className={styles.background} />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Добро пожаловать в Хогвартс!
