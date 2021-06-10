fb735571c5f5d81e9cb2cbd4a38779d4b0e5e399
Kate-chernyuk <76167446+Kate-chernyuk@users.noreply.github.com>
2021-06-10 09:54:34 +0300

    Update article.js

--- components/article.js
+++ components/article.js
@@ -2,10 +2,8 @@ import styles from '../styles/blog.module.css'
 
 export default function Article({children}) {
   return(
-    <>
     <div className={styes.article}>
         {children}
     </div>
-    </>
    )
 }
