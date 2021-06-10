f13967c1336a36c8b289fa4db87671be464106aa
Kate-chernyuk <76167446+Kate-chernyuk@users.noreply.github.com>
2021-06-10 09:52:17 +0300

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,26 +1,32 @@
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
+
 import PostTitle from '../../components/post-title'
 import Article from '../../components/article'
 import PostContent from '../../components/post-content'
+
 import content from '../../data/posts.json'
 
-const Post = (post) => {
+const Post = ( post ) => {
     const router = useRouter()
-    const {slug}  = router.query
+    const { slug } = router.query
 
     return (
+
         <Article>
             <PostTitle post={post}> </PostTitle>
             <PostContent content={post.content} />
         </Article>
+
     )
 }
 
 export async function getStaticPaths() {
     const posts = content.posts
+
     const paths = posts.map((post) => ({
         params: {slug: post.slug}
     }))
+
     return {paths, fallback: false}
 }
 
@@ -28,6 +34,7 @@ export async function getStaticProps( {params} ) {
     const currentPost = content.posts.find(post => {
         return post.slug == params.slug
     })
+
     return { props: currentPost }
 }
 
