74d37b28ec0a2a05ab50435f76d4dd87cae19967
Kate-chernyuk <76167446+Kate-chernyuk@users.noreply.github.com>
2021-06-10 09:57:52 +0300

    Update post-title.js

--- components/post-title.js
+++ components/post-title.js
@@ -1,9 +1,18 @@
 import styles from '../styles/blog.module.css'
 
-export default functionPostTitle({post}) {
-  return (
-    <div className={styles.postInfo}>
-    <h1>{post.title}</h1>
-    </div>
-  )
-}
+export default function PostTitle({ post }) {
+    return (
+        <>
+          <div className={styles.postInfo}>
+
+            <h1>
+              { post.title }
+            </h1>
+
+            Author: <div className={styles.author}>  { post.author } </div>
+
+          </div>
+
+      </>
+    )
+  }
